3f3ba4fe1fd8681740ec010d1a54163255864656
Nina Turner <cfratto87@gmail.com>
2023-05-31 15:42:23 -0500

    starting menue page

---
+++ src/menu.js
@@ -0,0 +1,32 @@
+function createMenu() {
+    const menu = document.createElement('div');
+    menu.classList.add('menu-container')
+}
+   
+   
+   
+   <div id="menu-container">
+        <div id="beverages">
+            <h2>Beverages</h2>
+            <p id="bev-option">Latte</p>
+            <p id="bev-option">Drip Coffee</p>
+            <p id="bev-option">Hot Chocolate</p>
+            <p id="bev-option">Orange Juice</p>
+            <p id="bev-option">Milk</p>
+            <p id="bev-option">Sodas</p>
+        </div>
+        <div id="food">
+            <h2>Meals</h2>
+            <p id="food-option">Pancakes</p>
+            <p id="food-option">French Toast</p>
+            <p id="food-option">Waffles</p>
+            <p id="food-option">Classic Breakfast</p>
+            <p id="food-option">Egg Benedict</p>
+            <p id="food-option">Omlette</p>
+            <p id="food-option">Biscuits and Gravy</p>
+            <p id="food-option">Breakfast Sandwich</p>
+            <p id="food-option">Breakfast Tacos</p>
+            <p id="food-option">Chilaquiles</p>
+            <p id="food-option">Migas</p>
+        </div>
+    </div>
